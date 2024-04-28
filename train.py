0c71ea87f79b124a9ad80a60c028618928a89b8f
dbuzinski <davidbuzinski@gmail.com>
2024-04-28 16:47:12 +0000

    update almost working

--- train.py
+++ train.py
@@ -1,37 +1,49 @@
+import os
 import torch
-from heroicml import use_plugin
 from model import GPTConfig, GPT
+from opal.metrics.loss import Loss
 from data import OpenWebTextDataLoader
 
-use_plugin("pytorch")
+# config
+torch.manual_seed(1337)
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+
+plugins = ["pytorch", "training_progress"]
+
+def model(hyperparameters):
+    config = GPTConfig(block_size=hyperparameters["block_size"],
+                       vocab_size=hyperparameters["vocab_size"],
+                       n_layer=hyperparameters["n_layer"],
+                       n_head=hyperparameters["n_head"],
+                       n_embd=hyperparameters["n_embd"],
+                       dropout=hyperparameters["dropout"],
+                       bias=hyperparameters["bias"])
+    m = GPT(config)
+    m.compile()
+    m.to('cuda')
+    return m
 
 
-def model(session):
-    hyperparameters = session.hyperparameters
-    config = GPTConfig(**hyperparameters)
-    return GPT(config)
-
-
-def data(session):
-    batch_size = session.hyperparameters["batch_size"]
+def data(hyperparameters):
+    batch_size = hyperparameters["batch_size"]
     training_set = "data/openwebtext/train.txt"
     validation_set = "data/openwebtext/valid.txt"
     training_data = OpenWebTextDataLoader(training_set, batch_size=batch_size, shuffle=True)
     validation_data = OpenWebTextDataLoader(validation_set, batch_size=batch_size, shuffle=False)
-    return {"training_data": training_data, "validation_data": validation_data}
+    return {"training": training_data, "validation": validation_data}
 
 
-def optimizer(session):
-    model = session.model
-    hyperparameters = session.hyperparameters
+def optimizer(hyperparameters):
+    m = model(hyperparameters)
     weight_decay = hyperparameters["weight_decay"]
     learning_rate = hyperparameters["learning_rate"]
     betas = (hyperparameters["beta1"], hyperparameters["beta2"])
-    return model.configure_optimizers(weight_decay, learning_rate, betas)
+    return m.configure_optimizers(weight_decay, learning_rate, betas, 'cuda')
 
 
 loss = torch.nn.CrossEntropyLoss()
-
+metrics = [Loss(loss)]
 
 hyperparameters = {
     "gradient_accumulation_steps": 5*8,
@@ -52,6 +64,7 @@ hyperparameters = {
     "warmpup_iters": 2000,
     "lr_decay_iters": 600000,
     "min_lr": 6e-5,
+    "vocab_size": 50304,
 }
 
 ### plugin configurations ###
